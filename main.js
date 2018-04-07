4433240c198e93e27a1bfcb12c77bb7eb513fd5e
Unknown <charlescollings@usa.net>
2018-04-07 13:08:06 -0500

    initial file structure

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+
+
+
+
+
+    $("#train-table > tbody").append('<tr><td>' + '3:05pm' + '</td><td>' + 'Milw' + '</td><td>' + '47%' + '</td><td>' + '43.3%' + '</td><td>' + '(+131)' + '</td><td>' + '+3.7%' + '</td></tr>');
+    // need to substitute scraped data for hard coded values that are in here righ now
+});
